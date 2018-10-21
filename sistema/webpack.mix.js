8ac39a0caef1803ca89aad50554b88713a0da6d9
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2018-10-20 22:55:12 -0500

    Proyecto sistema
     Laravel Mix, compilando archivos CSS, JavaScript, Implementación de Vue.JS, Configurando el menú con VueJs

--- sistema/webpack.mix.js
+++ sistema/webpack.mix.js
@@ -11,5 +11,22 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    //Icons
+    'resources/plantilla/css/font-awesome.min.css',
+    'resources/plantilla/css/simple-line-icons.min.css',
+    //Main styles for this application
+    'resources/plantilla/css/style.css'
+], 'public/css/plantilla.css')
+.scripts([
+    //Bootstrap and necessary plugins
+    'resources/plantilla/js/jquery.min.js',
+    'resources/plantilla/js/popper.min.js',
+    'resources/plantilla/js/bootstrap.min.js',
+    'resources/plantilla/js/pace.min.js',
+    //Plugins and scripts required by all views
+    'resources/plantilla/js/Chart.min.js',
+    //GenesisUI main scripts
+    'resources/plantilla/js/template.js'
+], 'public/js/plantilla.js')
+.js (['resources/js/app.js'],'public/js/app.js');
